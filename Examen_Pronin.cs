c88793b2f26a225965f4bf5b2b1de9c0a53b4ac7
user3945 <Pronchik29@gmail.com>
2026-05-12 00:12:40 +0300

    .

---
+++ Examen_Pronin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class VowelCounter
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.InputEncoding = System.Text.Encoding.UTF8;
+
+        Console.Write("Введите строку: ");
+        string input = Console.ReadLine() ?? "";
+
+        string vowels = "аеёиоуыэюяАЕЁИОУЫЭЮЯ";
+
+        int count = 0;
+        foreach (char c in input)
+        {
+            if (vowels.Contains(c))
+                count++;
+        }
+
+        Console.WriteLine($"\nСтрока: \"{input}\"");
+        Console.WriteLine($"Количество гласных букв: {count}");
+    }
+}
